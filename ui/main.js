0dda2ebf59b594f66035355f8aa795cc6724387b
Abhya Tripathi <abhya.tripathi.at@gmail.com>
2018-02-18 11:24:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ var button = document.getElementById('counter');
 var counter=0;
 button.onclick = function(){
   counter = counter + 1;
-  var span= documentt.getElmentById('count');
+  var span= documentt.getElementById('count');
   span.innerHTML = counter.toString();
 };
