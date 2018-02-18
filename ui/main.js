5ed5b5062848d8c5282ced53f4ae9e0e94746e6c
Abhya Tripathi <abhya.tripathi.at@gmail.com>
2018-02-18 11:19:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,6 @@
-console.log('Loaded!');
-var img= document.getElementById('madi');
-marginLeft=0;
-function moveright()
-{
-    marginLeft= marginLeft + 1;
-    img.style.marginLeft= marginLeft+ 'px';
-}
-img.onclick= function() {
-   var interval = setInterval(moveright,50);
+var button = document.getElementById('counter');
+button.onclick = function(){
+  counter = counter + 1;
+  var span= documentt.getelmentById('count');
+  span.innerHTML = counter.toString();
 };
